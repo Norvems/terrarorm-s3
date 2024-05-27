c63866094f81962d89ce5a4785eacd2c24310278
Krzysztof Kaźmierczak <157293146+Norvems@users.noreply.github.com>
2024-05-27 18:02:08 +0200

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_access_key           = "test"
+  region                      = "us-east-1"
+  s3_force_path_style         = true
+
+  endpoints {
+    s3     = "http://localhost:4566"
+  }
+}
